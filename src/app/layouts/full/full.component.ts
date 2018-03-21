d261a29ec447c6229376aed1626d549e16679945
DESKTOP-2OU1GPU\nahid <nahiduzzaman@sgcsoft.net>
2018-03-22 02:03:28 +0600

    update

---
+++ src/app/layouts/full/full.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-full-layout',
+    templateUrl: './full.component.html',
+    styleUrls: ['./full.component.css']
+})
+export class FullComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+}
